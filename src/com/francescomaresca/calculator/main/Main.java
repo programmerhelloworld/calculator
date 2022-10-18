bc2a9505aa44bd8553fb99802debf517f26588eb
Francesco Maresca <97321140+programmerhelloworld@users.noreply.github.com>
2022-10-18 18:14:04 +0200

    Add files via upload

---
+++ src/com/francescomaresca/calculator/main/Main.java
@@ -0,0 +1,359 @@
+package com.francescomaresca.calculator.main;
+
+import javax.script.ScriptEngineManager;
+
+import org.python.core.PyObject;
+import org.python.util.PythonInterpreter;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+public class Main {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JButton btnNewButton_1;
+	private JButton btnNewButton_2;
+	private JButton btnNewButton_3;
+	private JButton btnNewButton_4;
+	private JButton btnNewButton_5;
+	private JButton btnNewButton_6;
+	private JButton btnNewButton_7;
+	private JButton btnNewButton_8;
+	private JButton btnNewButton_9;
+	private JButton btnNewButton_10;
+	private JButton btnNewButton_11;
+	private JButton btnNewButton_12;
+	private JButton btnNewButton_13;
+	private JButton btnNewButton_14;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		PythonInterpreter pyth = new PythonInterpreter();
+
+		JFrame frame = new JFrame("Francesco Maresca's Calculator (Part of Maresca Math Tools)");
+		frame.setVisible(true);
+		frame.getContentPane().setBackground(Color.WHITE);
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 424, 379);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JTextField textField = new JTextField();
+		textField.setBackground(Color.LIGHT_GRAY);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("1");
+		btnNewButton.setBackground(Color.LIGHT_GRAY);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String a = textField.getText();
+				textField.setText(a.concat("1"));
+			}
+		});
+		
+		JButton btnNewButton_1 = new JButton("2");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String a = textField.getText();
+				textField.setText(a.concat("2"));
+			}
+		});
+		btnNewButton_1.setBackground(Color.LIGHT_GRAY);
+		
+		JButton btnNewButton_2 = new JButton("3");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String a = textField.getText();
+				textField.setText(a.concat("3"));
+			}
+		});
+		btnNewButton_2.setBackground(Color.LIGHT_GRAY);
+		
+		JButton btnNewButton_3 = new JButton("7");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String a = textField.getText();
+				textField.setText(a.concat("7"));
+			}
+		});
+		btnNewButton_3.setBackground(Color.LIGHT_GRAY);
+		
+		JButton btnNewButton_4 = new JButton("8");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String a = textField.getText();
+				textField.setText(a.concat("8"));
+			}
+		});
+		btnNewButton_4.setBackground(Color.LIGHT_GRAY);
+		
+		JButton btnNewButton_5 = new JButton("9");
+		btnNewButton_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String a = textField.getText();
+				textField.setText(a.concat("9"));
+			}
+		});
+		btnNewButton_5.setBackground(Color.LIGHT_GRAY);
+		
+		JButton btnNewButton_6 = new JButton("4");
+		btnNewButton_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String a = textField.getText();
+				textField.setText(a.concat("4"));
+			}
+		});
+		btnNewButton_6.setBackground(Color.LIGHT_GRAY);
+		
+		JButton btnNewButton_7 = new JButton("5");
+		btnNewButton_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String a = textField.getText();
+				textField.setText(a.concat("5"));
+			}
+		});
+		btnNewButton_7.setBackground(Color.LIGHT_GRAY);
+		
+		JButton btnNewButton_8 = new JButton("6");
+		btnNewButton_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String a = textField.getText();
+				textField.setText(a.concat("6"));
+			}
+		});
+		btnNewButton_8.setBackground(Color.LIGHT_GRAY);
+		
+		JButton btnNewButton_9 = new JButton("0");
+		btnNewButton_9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String a = textField.getText();
+				textField.setText(a.concat("0"));
+			}
+		});
+		btnNewButton_9.setBackground(Color.LIGHT_GRAY);
+		
+		JButton btnNewButton_10 = new JButton("+");
+		btnNewButton_10.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String a = textField.getText();
+				textField.setText(a.concat("+"));
+			}
+		});
+		btnNewButton_10.setBackground(Color.LIGHT_GRAY);
+		
+		JButton btnNewButton_11 = new JButton("-");
+		btnNewButton_11.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String a = textField.getText();
+				textField.setText(a.concat("-"));
+			}
+		});
+		btnNewButton_11.setBackground(Color.LIGHT_GRAY);
+		
+		JButton btnNewButton_12 = new JButton("X");
+		btnNewButton_12.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String a = textField.getText();
+				textField.setText(a.concat("*"));
+			}
+		});
+		btnNewButton_12.setBackground(Color.LIGHT_GRAY);
+		
+		JButton btnNewButton_13 = new JButton("/");
+		btnNewButton_13.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String a = textField.getText();
+				textField.setText(a.concat("/"));
+			}
+		});
+		JTextField textField_1 = new JTextField();
+		textField_1.setEditable(false);
+		textField_1.setColumns(10);
+		textField_1.setBackground(Color.LIGHT_GRAY);
+		
+		btnNewButton_13.setBackground(Color.LIGHT_GRAY);
+		
+		JButton btnNewButton_14 = new JButton("=");
+		btnNewButton_14.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				
+			
+					
+			String expression = textField.getText();
+			
+		/*
+				if (expression.contains(":")){
+					
+					String expression2 = expression.replace(':', '/');
+					//expression2 = expression;
+					System.out.println(expression2);
+					expression2 = expression;
+					
+				} else {
+					
+					expression = expression;
+					
+				}
+				
+				*/
+			
+			PyObject pythex;
+			
+			pythex = pyth.eval(expression);
+				  
+			String pythexpr = pythex.toString();
+			
+			textField_1.setText(pythexpr);
+				
+				
+				
+			    
+			    //System.out.println(engine.eval(sum));
+			}
+		});
+		
+		JButton btnNewButton_15 = new JButton("Clear");
+		btnNewButton_15.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText("");
+				textField_1.setText("");
+			}
+		});
+		
+		JButton btnNewButton_11_1 = new JButton("(");
+		btnNewButton_11_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String a = textField.getText();
+				textField.setText(a.concat("("));
+			}
+		});
+		btnNewButton_11_1.setBackground(Color.LIGHT_GRAY);
+		
+		JButton btnNewButton_11_1_1 = new JButton(")");
+		btnNewButton_11_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String a = textField.getText();
+				textField.setText(a.concat(")"));
+			}
+		});
+		btnNewButton_11_1_1.setBackground(Color.LIGHT_GRAY);
+		
+		
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(38)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(textField, GroupLayout.PREFERRED_SIZE, 167, GroupLayout.PREFERRED_SIZE)
+										.addGroup(groupLayout.createSequentialGroup()
+											.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+											.addPreferredGap(ComponentPlacement.UNRELATED)
+											.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+											.addGap(18)
+											.addComponent(btnNewButton_2, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)))
+									.addGap(49)
+									.addComponent(btnNewButton_14))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(btnNewButton_6, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+									.addGap(12)
+									.addComponent(btnNewButton_7, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+									.addGap(18)
+									.addComponent(btnNewButton_8, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(btnNewButton_3, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addGroup(groupLayout.createSequentialGroup()
+											.addComponent(btnNewButton_9, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+											.addPreferredGap(ComponentPlacement.RELATED, 204, Short.MAX_VALUE)
+											.addComponent(btnNewButton_15))
+										.addGroup(groupLayout.createSequentialGroup()
+											.addComponent(btnNewButton_4, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+											.addGap(18)
+											.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+												.addGroup(groupLayout.createSequentialGroup()
+													.addComponent(btnNewButton_13, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+													.addGap(77)
+													.addComponent(btnNewButton_11_1, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+													.addPreferredGap(ComponentPlacement.UNRELATED)
+													.addComponent(btnNewButton_11_1_1, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE))
+												.addComponent(btnNewButton_5, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE))
+											.addGap(68)))
+									.addPreferredGap(ComponentPlacement.RELATED))))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(15)
+							.addComponent(btnNewButton_10, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnNewButton_11, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(btnNewButton_12, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(24, GroupLayout.PREFERRED_SIZE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap(251, Short.MAX_VALUE)
+					.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 167, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnNewButton_14, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(32)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnNewButton)
+								.addComponent(btnNewButton_1)
+								.addComponent(btnNewButton_2))
+							.addGap(18)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnNewButton_6)
+								.addComponent(btnNewButton_8)
+								.addComponent(btnNewButton_7))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnNewButton_3)
+								.addComponent(btnNewButton_4)
+								.addComponent(btnNewButton_5))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnNewButton_9)
+							.addPreferredGap(ComponentPlacement.RELATED, 62, Short.MAX_VALUE)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnNewButton_10)
+								.addComponent(btnNewButton_11)
+								.addComponent(btnNewButton_12)
+								.addComponent(btnNewButton_13)
+								.addComponent(btnNewButton_11_1_1)
+								.addComponent(btnNewButton_11_1)))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(98)
+							.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(43)
+							.addComponent(btnNewButton_15)))
+					.addContainerGap())
+		);
+		frame.getContentPane().setLayout(groupLayout);
+	}
+}
